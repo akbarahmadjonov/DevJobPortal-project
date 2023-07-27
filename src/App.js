a28e960dd8f27f7b63d60d8e1d4232f8afee974b
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-07-27 17:26:48 +0500

    Jobs page in

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Register from "./Pages/Auth/Register";
 // import "./main.scss";
 import "./index.css";
 import Header from "./Widgets/Header/Header";
+import { Jobs } from "./Pages/Jobs/Jobs";
 
 const App = () => (
   <>
-  <main>
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/auth" element={<Register />}>
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </div>
+    <main>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/auth" element={<Register />}>
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route path="/jobs" element={<Jobs />} />
+        </Routes>
+      </div>
     </main>
   </>
 );
