c44ffac878a5bc140e4303b5f512e8c25407e4a9
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-08-01 11:00:25 +0500

    updated, bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./Pages/Auth/Login";
-import Register from "./Pages/Auth/Register";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import "./main.scss";
-import Header from "./Widgets/Header/Header";
 import { Home } from "./Pages";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
 import { Jobs } from "./Pages/Jobs/Jobs";
 
 const App = () => {
   return (
     <main>
       <div>
-        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
